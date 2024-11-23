2f1a99f61431e8b776449e6755ff557742358f9a
My comp <My comp@DESKTOP-HBDEDEI>
2024-11-23 13:40:12 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lr10.Models;
+
+namespace lr10.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new ConsultationModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(ConsultationModel model)
+        {
+            _logger.LogInformation($"Отримано дані: {model.FullName}, {model.Email}, {model.ConsultationDate}");
+
+            if (ModelState.IsValid)
+            {
+                TempData["SuccessMessage"] = "Реєстрація успішна!";
+                return View("Success", model);
+            }
+
+            foreach (var modelError in ModelState.Values.SelectMany(v => v.Errors))
+            {
+                _logger.LogError($"Помилка валідації: {modelError.ErrorMessage}");
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/ConsultationModel.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lr10.Models
+{
+    public class ConsultationModel
+    {
+        [Required(ErrorMessage = "Будь ласка, введіть ім'я та прізвище")]
+        [Display(Name = "Ім'я та прізвище")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Будь ласка, введіть email")]
+        [EmailAddress(ErrorMessage = "Некоректний формат email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Будь ласка, виберіть дату консультації")]
+        [Display(Name = "Дата консультації")]
+        [DataType(DataType.Date)]
+        [FutureWorkingDayValidation(ErrorMessage = "Дата має бути майбутньою робочою датою")]
+        public DateTime ConsultationDate { get; set; }
+
+        [Required(ErrorMessage = "Будь ласка, виберіть продукт")]
+        [Display(Name = "Продукт")]
+        public string Product { get; set; }
+
+        public static readonly string[] AvailableProducts = new[]
+        {
+            "JavaScript",
+            "C#",
+            "Java",
+            "Python",
+            "Основи"
+        };
+    }
+
+    public class FutureWorkingDayValidationAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime date)
+            {
+                if (date.Date <= DateTime.Now.Date)
+                    return false;
+
+                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                    return false;
+
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class NoBasicsOnMondayAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var model = (ConsultationModel)validationContext.ObjectInstance;
+
+            if (model.Product == "Основи" && model.ConsultationDate.DayOfWeek == DayOfWeek.Monday)
+            {
+                return new ValidationResult("Консультації з 'Основи' не проводяться по понеділках");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
